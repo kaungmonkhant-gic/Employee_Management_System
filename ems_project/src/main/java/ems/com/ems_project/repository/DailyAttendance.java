c04d1e74a2c64fb48251f62ec434d6997cf232f9
HtetLinn-GIC <htetlinn@gicjp.com>
2025-01-08 12:37:18 +0630

    Error Fix

--- ems_project/src/main/java/ems/com/ems_project/repository/DailyAttendance.java
+++ ems_project/src/main/java/ems/com/ems_project/repository/DailyAttendance.java
@@ -1,9 +1,8 @@
 package ems.com.ems_project.repository;
 
-import ems.com.ems_project.model.EmpDailyAtts;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EmpDailyAttsRepository extends JpaRepository<EmpDailyAtts, Integer> {
+public interface DailyAttendance extends JpaRepository<ems.com.ems_project.model.DailyAttendance, Integer> {
 }
