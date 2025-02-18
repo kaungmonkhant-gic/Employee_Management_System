48cc7178c67130b80c2cf26b22cf050a3513299f
archeryuki <hninsandarhlaing15@gmail.com>
2025-02-18 23:38:15 +0630

    enum edit

--- ems_project/src/main/java/ems/com/ems_project/model/LeaveType.java
+++ ems_project/src/main/java/ems/com/ems_project/model/LeaveType.java
@@ -1,5 +1,8 @@
 package ems.com.ems_project.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum LeaveType {
     ANNUAL("Annual Leave"),
     CASUAL("Casual Leave"),
@@ -15,5 +18,19 @@ public enum LeaveType {
     public String getDisplayName() {
         return displayName;
     }
-}
 
+    @JsonValue // Converts ENUM to JSON (e.g., "ANNUAL" -> "Annual Leave")
+    public String toJson() {
+        return displayName;
+    }
+
+    @JsonCreator // Converts JSON string back to ENUM (e.g., "Annual Leave" -> ANNUAL)
+    public static LeaveType fromJson(String value) {
+        for (LeaveType type : LeaveType.values()) {
+            if (type.displayName.equalsIgnoreCase(value)) {
+                return type;
+            }
+        }
+        throw new IllegalArgumentException("Invalid LeaveType: " + value);
+    }
+}
