c51708c43dacdd23afaf3ccff15d65d71a716ac6
archeryuki <hninsandarhlaing15@gmail.com>
2025-03-07 02:17:01 +0630

    ot reject solved

--- ems_project/src/main/java/ems/com/ems_project/service/OtService.java
+++ ems_project/src/main/java/ems/com/ems_project/service/OtService.java
@@ -155,7 +155,8 @@ public class OtService {
             if ("approve".equalsIgnoreCase(action)) {
                 ot.setStatus(RequestStatus.APPROVED);
                 ot.setRejectionReason(null);  // Clear rejection reason if approving
-            } else {
+            } else if ("reject".equalsIgnoreCase(action)) {
+                // If rejecting, a rejection reason must be provided
                 if (rejectionReason == null || rejectionReason.trim().isEmpty()) {
                     throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Rejection reason is required.");
                 }
@@ -255,8 +256,6 @@ public class OtService {
             return principal.toString();
         }
     }
-
-
 
     public String generateOtId() {
         // Get the last Attendance ID from the database
