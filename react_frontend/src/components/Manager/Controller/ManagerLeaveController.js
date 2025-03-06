590755a0f44fb7b464e428d07be1dc94e84f706f
KhinHtetHtetOo <khinhtet.km@gmail.com>
2025-03-06 21:15:19 +0630

    Manager self leave request

---
+++ react_frontend/src/components/Manager/Controller/ManagerLeaveController.js
@@ -0,0 +1,49 @@
+// Code for Leave Request Controller
+import leaveService from "../Service/ManagerLeaveService";
+import { getRemainingLeaveDays } from "../Service/LeaveRequestService";
+
+
+const ManagerLeaveRequestController = {
+
+    fetchLeaveCounts: async (setPending, setApproved, setRejected) => {
+        try {
+          const data = await leaveService.getLeaveCounts();
+          setPending(data.PENDING);
+          setApproved(data.APPROVED);
+          setRejected(data.REJECTED);
+        } catch (error) {
+          console.error("Failed to fetch leave counts", error);
+        }
+      },
+    
+      submitLeaveRequest: async (leaveData, onSuccess, onError) => {
+        try {
+          const response = await leaveService.applyForLeave(leaveData);
+          onSuccess(response);
+        } catch (error) {
+          onError(error);
+        }
+      },
+
+      getLeaveBalance: async () => {
+        const data = await leaveService.fetchLeaveBalanceFromAPI();
+        if (!data) return { annualLeave: 0, casualLeave: 0, medicalLeave: 0 };
+        
+        return {
+          annualLeave: data.annualLeave,
+          casualLeave: data.casualLeave,
+          medicalLeave: data.medicalLeave,
+        };
+      },
+
+      fetchRemainingLeaveDays: async (leaveType) => {
+        const leaveBalance = await leaveService.fetchLeaveBalanceFromAPI();
+        if (leaveBalance) {
+          return leaveService.getRemainingLeaveDays(leaveType, leaveBalance);
+        }
+        return 0;
+      },
+  
+};
+
+export default ManagerLeaveRequestController;
---
+++ react_frontend/src/components/Manager/ManagerLeave.js
@@ -0,0 +1,99 @@
+import React, { useState, useEffect } from "react";
+import { BellFill, CheckCircleFill } from "react-bootstrap-icons";
+import { Modal, Button } from "react-bootstrap";
+import LeaveForm from "../common/LeaveForm";
+import apiClient from "../api/apiclient";
+import { useNavigate } from "react-router-dom";
+
+const LeaveRequests = () => {
+  const [pending, setPending] = useState([]);
+  const [approved, setApproved] = useState([]);
+  const [rejected, setRejected] = useState(0);
+  const [showModal, setShowModal] = useState(false);
+  const [availableLeaveDays, setAvailableLeaveDays] = useState(0);
+  const navigate = useNavigate();
+
+
+  useEffect(() => {
+    const fetchLeaveRequests = async () => {
+      try {
+        const response = await apiClient.get("/leave/status-count"); // pending / approved / rejected
+        setPending(response.data.PENDING);
+        setApproved(response.data.APPROVED);
+        setRejected(response.data.REJECTED);
+      } catch (error) {
+        console.error("Error fetching leave requests:", error);
+      }
+    };
+
+    fetchLeaveRequests();
+  }, []);
+  
+  return (
+    <div className="container mt-4">
+      <div className="row mb-4">
+        <div className="col-md-4">
+          <div className="d-flex align-items-center p-3 border rounded shadow-sm" style={{ backgroundColor: "#fff" }}>
+            <BellFill size={32} color="orange" />
+            <div className="ms-3">
+              <p className="text-muted mb-1">Pending Requests</p>
+              <p className="fw-bold mb-0">{pending}</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-md-4">
+          <div className="d-flex align-items-center p-3 border rounded shadow-sm" style={{ backgroundColor: "#fff" }}>
+            <CheckCircleFill size={32} color="green" />
+            <div className="ms-3">
+              <p className="text-muted mb-1">Approved Requests</p>
+              <p className="fw-bold mb-0">{approved}</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-md-4">
+          <div className="d-flex align-items-center p-3 border rounded shadow-sm" style={{ backgroundColor: "#danger" }}>
+            <CheckCircleFill size={32} color="red" />
+            <div className="ms-3">
+              <p className="text-muted mb-1">Rejected Requests</p>
+              <p className="fw-bold mb-0">{rejected}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="p-3 border rounded shadow-sm bg-white">
+        <h5 className="mb-3">Leave Requests</h5>
+        {/* <p>Available Leave Balance: {availableLeaveDays} days</p> */}
+        <Button variant="secondary" onClick={() => setShowModal(true)}>
+          Apply for leave
+        </Button>
+        {/* <button className="btn btn-primary mb-3">
+        View Confirmed Requests
+      </button> */}
+        <Button variant="secondary" className="ms-3" onClick={() => navigate("/manager-dashboard/manager-leave-approval")}
+        >
+          Incoming Leave Requests
+        </Button>
+        <Button variant="secondary" className="ms-3" onClick={() => navigate("/manager-dashboard/leave-confirmed")}>
+          Confirmed Requests
+        </Button>
+
+      </div>
+
+      {/* Leave Form Modal */}
+      <Modal show={showModal} onHide={() => setShowModal(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Leave Request Form</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <LeaveForm/>
+           {/* called the leave form in common file */}
+        </Modal.Body>
+      </Modal>
+    </div>
+  );
+};
+
+export default LeaveRequests;
