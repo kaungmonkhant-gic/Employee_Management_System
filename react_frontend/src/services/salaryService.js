8adb8e20c0d85af516156c66e8111bf6e7113d20
Hsuthet <hsuthetpaingtun@gmail.com>
2025-03-20 10:11:16 +0630

    Calculate Salary

--- react_frontend/src/services/salaryService.js
+++ react_frontend/src/services/salaryService.js
@@ -2,18 +2,16 @@ import apiClient from "../components/api/apiclient";
 
 const salaryService = {
 
-     fetchLeaves : async () => {
+     fetchSalary : async () => {
         try {
         
-            const response = await apiClient.get("/salary/all"); // Replace "/leaves" with your actual endpoint
+            const response = await apiClient.get("salary/data?year={year}&month={month}"); // Replace "/leaves" with your actual endpoint
             return Array.isArray(response.data) ? response.data : [];
         } catch (error) {
             console.error("Error fetching salaries:", error);
             return [];
         }
-      
     },
-
 
   calculateSalary: async (salaryData) => {
     try {
