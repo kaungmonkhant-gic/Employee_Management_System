a1c329fd5d59a61d4b56cbfbe940ef17833873a4
Hsuthet <hsuthetpaingtun@gmail.com>
2025-03-20 11:20:11 +0630

    salary service and salary controller

--- react_frontend/src/services/salaryService.js
+++ react_frontend/src/services/salaryService.js
@@ -2,6 +2,17 @@ import apiClient from "../components/api/apiclient";
 
 const salaryService = {
 
+  fetchSalaryData : async () => {
+    try {
+    
+        const response = await apiClient.get("/salary/all"); // Replace "/leaves" with your actual endpoint
+        return Array.isArray(response.data) ? response.data : [];
+    } catch (error) {
+        console.error("Error fetching salaries:", error);
+        return [];
+    }
+  
+},
      fetchSalary : async () => {
         try {
         
